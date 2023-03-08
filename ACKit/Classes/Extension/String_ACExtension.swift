1db055bf7b67a394a0826f65eafeac0342b6e7a6
gaoyuan <707216098@qq.com>
2023-03-08 16:26:12 +0800

    add

--- ACKit/Classes/Extension/String_ACExtension.swift
+++ ACKit/Classes/Extension/String_ACExtension.swift
@@ -7,6 +7,62 @@
 
 import UIKit
 
+//MARK: - 手机号和邮箱号处理
 extension String {
     
+    /// 匹配手机号
+    public func ac_matchPhoneNumber() -> Bool {
+        let pred : NSPredicate = NSPredicate.init(format: "self matches %@", "^1[3456789]\\d{9}$")
+        if pred.evaluate(with: self) {
+            return true
+        }
+        return false
+    }
+    
+    /// 匹配邮箱
+    public func ac_matchMailString() -> Bool {
+        let pred : NSPredicate = NSPredicate.init(format: "self matches %@", "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$")
+        if pred.evaluate(with: self) {
+            return true
+        }
+        return false
+    }
+    
+    /// 手机号脱敏显示，中间4位用*代替，比如185****8888
+    public func ac_fetchSecrectPhoneNumber() -> String {
+        if ac_matchPhoneNumber() && self.count >= 11 {
+            let starts = "****"
+            let starNum = starts.count
+            let beginOffset = Int(self.count / 2) - starNum/2
+            let startIndex = self.index(self.startIndex, offsetBy:beginOffset)
+            let endIndex = self.index(self.startIndex, offsetBy:(beginOffset + starNum))
+            let range = startIndex..<endIndex
+            var phone = self
+            phone.replaceSubrange(range, with: starts)
+            return phone
+        }
+        return self
+    }
+    
+    /// 邮箱脱敏显示，邮箱用4位*代替
+    public func ac_fetchSecrectEmail() -> String {
+        if ac_matchMailString() {
+            let emailComponents = self.components(separatedBy: "@")
+            guard var availableNum = emailComponents.first, let availableAddress = emailComponents.last else { return self }
+            if availableNum.count < 4 {
+                let r = ("***@" + availableAddress)
+                return r
+            }
+            let starts = "****"
+            let starNum = starts.count
+            let beginOffset = Int(availableNum.count / 2) - starNum/2
+            let startIndex = self.index(availableNum.startIndex, offsetBy:beginOffset)
+            let endIndex = self.index(availableNum.startIndex, offsetBy:beginOffset + starNum)
+            let range = startIndex..<endIndex
+            availableNum.replaceSubrange(range, with: "****")
+            let r = (availableNum + "@" + availableAddress)
+            return r
+        }
+        return self
+    }
 }
---
+++ ACKit/Classes/Extension/UIColor_ACExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor_ACExtension.swift
+//  ACKit
+//
+//  Created by gaoyuan on 2023/3/8.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    /// 将16进制颜色转化为RGB颜色
+    public static func ac_rgbValue(_ hexColor: String) -> (CGFloat, CGFloat, CGFloat) {
+        //先把#替换了
+        let hex = hexColor.replacingOccurrences(of: "#", with: "")
+        if hex.count != 6 {
+            return (0, 0, 0)
+        }
+        let text = Scanner.init(string: hex)
+        var colorValue:UInt64 = 0
+        text.scanHexInt64(&colorValue)
+        var red:CGFloat = 0
+        var green:CGFloat = 0
+        var blue:CGFloat = 0
+        red = CGFloat((colorValue & 0xFF0000)>>16)
+        green = CGFloat((colorValue & 0x00FF00)>>8)
+        blue = CGFloat((colorValue & 0x0000FF))
+        return (red, green, blue)
+    }
+
+}
